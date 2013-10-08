e660c33878d03ad56566b127639aded170609c83
zrma <bulbitain@nhnnext.org>
2013-10-07 20:44:32 -0700

    C# Wave Generator Source upload

---
+++ WaveFun/Main.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WaveFun
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerateWave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerateWave
+            // 
+            this.btnGenerateWave.Location = new System.Drawing.Point(12, 12);
+            this.btnGenerateWave.Name = "btnGenerateWave";
+            this.btnGenerateWave.Size = new System.Drawing.Size(241, 50);
+            this.btnGenerateWave.TabIndex = 1;
+            this.btnGenerateWave.Text = "Generate Sine Wave";
+            this.btnGenerateWave.UseVisualStyleBackColor = true;
+            this.btnGenerateWave.Click += new System.EventHandler(this.btnGenerateWave_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 74);
+            this.Controls.Add(this.btnGenerateWave);
+            this.Name = "frmMain";
+            this.Text = "Wave Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateWave;
+    }
+}
+
---
+++ WaveFun/Main.cs
@@ -0,0 +1,33 @@
+﻿//-----------------------------------------------------
+// Main.cs
+// Code for the main form of this app.
+//
+// Copyright (c) 2009 Dan Waters
+// This code is provided AS-IS and is written by me,
+// with no endorsement from Microsoft.
+//-----------------------------------------------------
+
+using System;
+using System.Windows.Forms;
+using System.Media;
+
+namespace WaveFun
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerateWave_Click(object sender, EventArgs e)
+        {
+            string filePath = @"C:\Users\Dan\Desktop\test2.wav";
+            WaveGenerator wave = new WaveGenerator(WaveExampleType.ExampleSineWave);
+            wave.Save(filePath);            
+
+            SoundPlayer player = new SoundPlayer(filePath);               
+            player.Play();
+        }
+    }
+}
